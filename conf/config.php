8af535baf8989abbe7a83753d220f7511101efa2
leeylong <creativeboyme@outlook.com>
2018-10-16 22:49:18 +0800

    整理完了index模板

---
+++ conf/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    //显示错误信息
+    'show_error_msg'          => true,
+    // 默认模块名
+    'default_module'         => 'admin',
+    // 视图输出字符串内容替换
+    'view_replace_str'       => [
+        '__IMG__'               =>'/static/img'
+    ],
+];
